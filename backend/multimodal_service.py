df6525cba81b0cb9390331c0eb5b951a966b2401
Somil Negi <somilnegi12345@gmail.com>
2026-04-24 16:26:32 +0530

    fix allow origins

--- backend/multimodal_service.py
+++ backend/multimodal_service.py
@@ -42,7 +42,7 @@ app = FastAPI(title="Multimodal Analysis Service", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-interview-prep-app-9yqf.onrender.com", "https://ai-interview-prep-app-blue.vercel.app/"],
+    allow_origins=["*"],
     allow_methods=["POST", "GET"],
     allow_headers=["*"],
 )
